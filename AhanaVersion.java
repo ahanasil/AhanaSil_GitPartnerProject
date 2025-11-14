7c0227bb7418b5bd72819319556528944bbbcaf5
aanyashridhar <aanyashridhar@gmail.com>
2025-11-14 14:00:14 -0500

    Added method createSquareArray
    createSquareArray creates a 2D array with size according to user specification

--- AhanaVersion.java
+++ AhanaVersion.java
@@ -1,3 +1,18 @@
+import java.lang.Math;
+
 public class AhanaVersion{
-  
+	public static void main(String[] args) {
+		//createSquareArray(4);
+	}
+
+  	public static void createSquareArray(int n) {
+		int[][] arr = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = (int)(Math.random()*10)+1;
+				//System.out.println(arr[i][j]);
+			}
+		}
+  }
 }
