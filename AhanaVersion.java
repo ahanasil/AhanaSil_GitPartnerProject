53cd30a5a8436fb6fbe55f37132dfe7139ed15c8
aanyashridhar <80227260+aanyashridhar@users.noreply.github.com>
2025-11-16 22:27:20 -0500

    created firstOddThirtyThree, also improved readability of old code
    firstOddThirtyThree is a method that returns the first odd in a given 2D array searching left-right whose left, right, top, & bottom neighbors have a sum greater than 33. returns -1 if no such element exists.
    
    Also improved the readability of the 2D array when it is printed, & improved efficiency of firstEvenSeventeen by only calculating neighborSum if element is even

--- AhanaVersion.java
+++ AhanaVersion.java
@@ -8,7 +8,10 @@ public class AhanaVersion{
 		System.out.println("First even: " + first_even);
 
 		int first_even_seventeen = firstEvenSeventeen(arr);
-		System.out.println("First even with neigbors greater than 17: " + first_even_seventeen);
+		System.out.println("First even with left-right neigbors sum greater than 17: " + first_even_seventeen);
+
+		int first_odd_thirty_three = firstOddThirtyThree(arr);
+		System.out.println("First odd with left-right & top-bottom neighbors sum greater than 33: " + first_odd_thirty_three);
 	}
 
   	public static int[][] createSquareArray(int n) {
@@ -17,8 +20,9 @@ public class AhanaVersion{
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				arr[i][j] = (int)(Math.random()*10)+1;
-				System.out.println(arr[i][j]);
+				System.out.print(arr[i][j] + "\t");
 			}
+			System.out.println();
 		}
 
 		return arr;
@@ -39,19 +43,50 @@ public class AhanaVersion{
 	public static int firstEvenSeventeen(int[][] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[0].length; j++) {
-				int even = -1;
+				int value = arr[i][j];
+				if (value % 2 != 0) {
+					continue;
+				}
+
 				int neighborSum = 0;
-				if (arr[i][j] % 2 == 0) {
-					even = arr[i][j];
+				if (j != 0) {
+					neighborSum += arr[i][j - 1];
+				}
+				if (j != arr[0].length - 1) {
+					neighborSum += arr[i][j + 1];
+				}
+				if (neighborSum > 17) {
+					return value;
+				}
+			}
+		}
+
+		return -1;
+	}
+
+	public static int firstOddThirtyThree(int[][] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				int value = arr[i][j];
+				if (value % 2 != 1) {
+					continue;
+				}
+
+				int neighborSum = 0;
+				if (i != 0) {
+					neighborSum += arr[i - 1][j];
+				}
+				if (i != arr.length - 1) {
+					neighborSum += arr[i + 1][j];
 				}
 				if (j != 0) {
 					neighborSum += arr[i][j - 1];
 				}
-				if (j != arr.length - 1) {
+				if (j != arr[0].length - 1) {
 					neighborSum += arr[i][j + 1];
 				}
-				if (even != -1 && neighborSum > 17) {
-					return even;
+				if (neighborSum > 33) {
+					return value;
 				}
 			}
 		}
