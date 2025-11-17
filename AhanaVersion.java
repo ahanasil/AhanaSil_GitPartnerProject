71397c9ed387bd4b95d1986a9bbd841e5ab87ca4
aanyashridhar <80227260+aanyashridhar@users.noreply.github.com>
2025-11-16 22:09:47 -0500

    created firstEvenSeventeen
    firstEvenSeventeen is a method that returns the first even number found in a 2D array (searching left-right top-down) that has left-right neighbors with a sum greater than 17. returns -1 if no such element exists.

--- AhanaVersion.java
+++ AhanaVersion.java
@@ -3,8 +3,12 @@ import java.lang.Math;
 public class AhanaVersion{
 	public static void main(String[] args) {
 		int[][] arr = createSquareArray(4);
+
 		int first_even = firstEven(arr);
-		//System.out.println("First even: " + first_even);
+		System.out.println("First even: " + first_even);
+
+		int first_even_seventeen = firstEvenSeventeen(arr);
+		System.out.println("First even with neigbors greater than 17: " + first_even_seventeen);
 	}
 
   	public static int[][] createSquareArray(int n) {
@@ -13,7 +17,7 @@ public class AhanaVersion{
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				arr[i][j] = (int)(Math.random()*10)+1;
-				//System.out.println(arr[i][j]);
+				System.out.println(arr[i][j]);
 			}
 		}
 
@@ -25,6 +29,29 @@ public class AhanaVersion{
 			for (int j = 0; j < arr[0].length; j++) {
 				if (arr[i][j] % 2 == 0) {
 					return arr[i][j];
+				}
+			}
+		}
+
+		return -1;
+	}
+
+	public static int firstEvenSeventeen(int[][] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				int even = -1;
+				int neighborSum = 0;
+				if (arr[i][j] % 2 == 0) {
+					even = arr[i][j];
+				}
+				if (j != 0) {
+					neighborSum += arr[i][j - 1];
+				}
+				if (j != arr.length - 1) {
+					neighborSum += arr[i][j + 1];
+				}
+				if (even != -1 && neighborSum > 17) {
+					return even;
 				}
 			}
 		}
